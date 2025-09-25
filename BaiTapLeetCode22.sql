3d8c1e752e0c955a64194cbfff7e77f28d9f3283
Hồ Quốc Cường <147083742+TopGermany@users.noreply.github.com>
2025-09-25 21:42:48 +0700

    Add files via upload

---
+++ BaiTapLeetCode22.sql
@@ -0,0 +1,30 @@
+﻿CREATE DATABASE BT_LEETCODE22
+
+USE BT_LEETCODE22
+
+-- 3. Tạo bảng Queue
+CREATE TABLE Queue (
+    person_id INT PRIMARY KEY,
+    person_name VARCHAR(50) NOT NULL,
+    weight INT NOT NULL,
+    turn INT NOT NULL
+);
+
+-- 4. Thêm dữ liệu
+INSERT INTO Queue (person_id, person_name, weight, turn) VALUES
+(5, 'Alice', 250, 1),
+(4, 'Bob', 175, 5),
+(3, 'Alex', 350, 2),
+(6, 'John Cena', 400, 3),
+(1, 'Winston', 500, 6),
+(2, 'Marie', 200, 4);
+
+-- 5. Xem dữ liệu
+WITH Sum_Weight AS (
+SELECT person_name,weight,SUM(weight) OVER(ORDER BY turn) AS Total_Weight  FROM Queue
+)
+SELECT TOP 1 person_name FROM Sum_Weight
+WHERE Total_Weight <= 1000
+ORDER BY Total_Weight DESC
+
+
---
+++ BaiTapLeetCode23.sql
@@ -0,0 +1,47 @@
+﻿-- 1. Tạo database
+CREATE DATABASE BT_LEETCODE23
+GO
+
+-- 2. Sử dụng database
+USE BT_LEETCODE23;
+GO
+
+-- 3. Tạo bảng Accounts
+CREATE TABLE Accounts (
+    account_id INT PRIMARY KEY,
+    income INT NOT NULL
+);
+GO
+
+-- 4. Chèn dữ liệu
+INSERT INTO Accounts (account_id, income) VALUES
+(3, 108939),
+(2, 12747),
+(8, 87709),
+(6, 91796);
+GO
+
+-- 5. Xem dữ liệu
+WITH Change_Income AS (
+SELECT *,CASE
+	WHEN income < 20000 THEN 'Low Salary'
+	WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+	WHEN income > 50000 THEN 'High Salary'
+	END AS category 
+FROM Accounts
+),
+-- Tạo bảng tạm thời(Mới)
+Temp_Table AS (
+SELECT 'Low Salary' AS Category
+union
+SELECT 'Average Salary' AS Category
+union
+SELECT 'High Salary' AS Category
+)
+SELECT A2.Category,COUNT(A1.category) AS accounts_count FROM Change_Income AS A1 
+	RIGHT JOIN Temp_Table AS A2
+ON A1.category = A2.Category
+GROUP BY  A2.Category 
+
+
+
